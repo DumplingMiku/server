c5c23821502ce07cc3f45f436f68203af1c842dd
DumplingMiku <44613282+DumplingMiku@users.noreply.github.com>
2020-03-29 00:30:12 +0900

    2020/03/29

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo apt install ngnix
+sudo apt install php7.2
+sudo apt install php-pear php7.2-curl php7.2-dev php7.2-gd php7.2-mbstring php7.2-zip php7.2-mysql php7.2-xml
+sudo apt install mariadb-server
