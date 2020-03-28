70ce32ea9f3d69d39eb4f27355107ecd2d9401c6
DumplingMiku <44613282+DumplingMiku@users.noreply.github.com>
2020-03-29 01:05:30 +0900

    2020/03/29

--- readme.md
+++ readme.md
@@ -2,8 +2,6 @@ install.sh
 安裝ngnix|安裝php7.2|安裝MariaDB  
 webmin.sh  
 安裝webmin後台  
-
-
-
 #給予使用者所有sql全縣  
 Grant All Privileges On *.* To ubuntu@localhost Identified By '123456' With Grant Option;  
+!!無法安裝重起，系統即可解決  
--- webmin.sh
+++ webmin.sh
@@ -2,4 +2,5 @@ sudo echo 'deb http://download.webmin.com/download/repository sarge contrib' >> 
 wget http://www.webmin.com/jcameron-key.asc
 sudo apt-key add jcameron-key.asc
 sudo apt update 
+sudo apt list --upgradable
 sudo apt install webmin 
