bc734edbca9c13235adf55396051462457e6547c
DumplingMiku <44613282+DumplingMiku@users.noreply.github.com>
2020-03-29 12:50:36 +0900

    2020/03/29 12:50

--- webmin.sh
+++ webmin.sh
@@ -3,7 +3,7 @@ if [[ `id -u` -eq 0 ]] ; then
     exit 1 ;
 fi
 
-sudo echo 'deb http://download.webmin.com/download/repository sarge contrib' >> /etc/apt/sources.list
+sudo sh -c "echo 'deb http://download.webmin.com/download/repository sarge contrib' >> /etc/apt/sources.list"
 wget http://www.webmin.com/jcameron-key.asc
 sudo apt-key add jcameron-key.asc
 sudo apt update 
