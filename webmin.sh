c4309ede64b5f4472ef935cf2b65f14cfa12d1e9
DumplingMiku <44613282+DumplingMiku@users.noreply.github.com>
2020-03-29 10:00:04 +0900

    2020/03/29

--- webmin.sh
+++ webmin.sh
@@ -1,4 +1,3 @@
-sudo su
 sudo echo 'deb http://download.webmin.com/download/repository sarge contrib' >> /etc/apt/sources.list
 wget http://www.webmin.com/jcameron-key.asc
 sudo apt-key add jcameron-key.asc
