3f70e6f72e90cd15d278743201b9d7dbffc386fc
DumplingMiku <44613282+DumplingMiku@users.noreply.github.com>
2020-04-01 22:37:21 +0900

    2020/04/01 21:37

--- mail.sh
+++ mail.sh
@@ -1,5 +1,5 @@
-wget https://codeload.github.com/iredmail/iRedMail/tar.gz/1.1
-tar zxvf iRedMail-1.1.tar.gz
+wget https://github.com/iredmail/iRedMail/archive/1.1.tar.gz
+tar zxvf 1.1.tar.gz
 sudo cp -r ./iRedMail-1.1 /var/www/html/mail
 sudo bash /var/www/html/mail/iRedMail.sh
 sudo rm -r ./iRedMail-1.1
