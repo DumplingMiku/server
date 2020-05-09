eca175d8d6262ae633ef644a10a37be0e104b42f
DumplingMiku <44613282+DumplingMiku@users.noreply.github.com>
2020-05-09 20:18:22 +0900

    20200509 1918

--- computer_exam_c/change.sh
+++ computer_exam_c/change.sh
@@ -7,5 +7,6 @@ sudo cp ~/server/computer_exam_c/cp/vsftpd.conf /etc/vsftpd.conf
 sudo cp ~/server/computer_exam_c/cp/telnet /etc/xinetd.d/telnet
 sudo cp ~/server/computer_exam_c/cp/.profile /root/.profile
 sudo cp ~/server/computer_exam_c/cp/lightdm.conf /etc/lightdm/lightdm.conf
+sudo cp ~/server/computer_exam_c/cp/dhcpd.conf /etc/dhcp/dhcpd.conf 
 sudo service vsftpd restart
 sudo service xinetd restart
