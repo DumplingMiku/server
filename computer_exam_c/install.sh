a65f779ba5beadf422ec69184ab24d4ef4ff8334
DumplingMiku <44613282+DumplingMiku@users.noreply.github.com>
2020-05-09 18:23:43 +0900

    20200509 1723

--- computer_exam_c/install.sh
+++ computer_exam_c/install.sh
@@ -1,1 +1,6 @@
+if [[ `id -u` -eq 0 ]] ; then
+    echo "Do not run this with sudo (do not run random things with sudo!)." ;
+    exit 1 ;
+fi
+
 sudo apt install apache2 vsftpd openssh-server xinetd telnetd
