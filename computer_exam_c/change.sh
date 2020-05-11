bd74bf4ff367acbe57d6140ee91af747c71d4dbe
DumplingMiku <44613282+DumplingMiku@users.noreply.github.com>
2020-05-11 21:31:43 +0900

    fix no root

--- computer_exam_c/change.sh
+++ computer_exam_c/change.sh
@@ -6,10 +6,10 @@ fi
 sudo cp ~/server/computer_exam_c/cp/vsftpd.conf /etc/vsftpd.conf 
 sudo cp ~/server/computer_exam_c/cp/telnet /etc/xinetd.d/telnet
 sudo cp ~/server/computer_exam_c/cp/.profile /root/.profile
-sudo cp ~/server/computer_exam_c/cp/lightdm.conf /etc/lightdm/lightdm.conf
+#sudo cp ~/server/computer_exam_c/cp/lightdm.conf /etc/lightdm/lightdm.conf
 sudo cp ~/server/computer_exam_c/cp/dhcpd.conf /etc/dhcp/dhcpd.conf
 sudo cp ~/server/computer_exam_c/clear.sh ~/Desktop/
-sudo cp -r ~/server/computer_exam_c/clear-root.sh /root/Desktop/
+#sudo cp -r ~/server/computer_exam_c/clear-root.sh /root/Desktop/
 sudo chmod 777 ~/Desktop/clear.sh
 sudo service vsftpd restart
 sudo service xinetd restart
