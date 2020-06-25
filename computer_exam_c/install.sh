4485f25d84ddf55c259c322d4753a5a2df4ced0e
Dumpling <ab60808@gmail.com>
2020-06-26 08:55:11 +0900

    rm computer_c_exam

--- computer_exam_c/install.sh
+++
@@ -1,9 +0,0 @@
-if [[ `id -u` -eq 0 ]] ; then
-    echo "Do not run this with sudo (do not run random things with sudo!)." ;
-    exit 1 ;
-fi
-
-sudo apt update
-sudo apt install apache2 vsftpd openssh-server xinetd telnetd isc-dhcp-server gedit net-tools -y
-sudo chmod 777 ~/server/computer_exam_c/
-~/server/computer_exam_c/change.sh
